205c4451f1aa4e2d431ef04fe6cb37f618ce85f6
Albin Issac <admin@albinsblog.com>
2020-06-19 22:23:32 -0500

    import sys

---
+++ scripts/wlst/ConfigureTLOGSSharedFileStore.py
@@ -0,0 +1,39 @@
+import sys
+
+print "@@@ Starting the script ..."
+
+from java.util import *
+from javax.management import *
+from java.io import FileInputStream
+
+print "@@@ Starting the script ..."
+global props
+ 
+#The directory of the domain configuration
+#/app/oracle/products/11g/admin/domains
+wlsDomain='C://Albin/SW/Oracle/Middleware/Oracle_Home/user_projects/domains/base_domain'
+print "WLSDOMAIN="+wlsDomain
+
+
+adminURL='t3://localhost:7001'
+adminUserName='weblogic'
+adminPassword='weblogic1'
+connect(adminUserName, adminPassword, adminURL)
+
+edit()
+startEdit()
+
+adminserverDir = File(wlsDomain+'/SOACoreDomain/soa_cluster/tlogs')
+bool = adminserverDir.mkdirs()
+
+cd('/Servers/MS1/DefaultFileStore/MS1')
+cmo.setDirectory(wlsDomain+'/SOACoreDomain/soa_cluster/tlogs')
+
+cd('/Servers/MS2/DefaultFileStore/MS2')
+cmo.setDirectory(wlsDomain+'/SOACoreDomain/soa_cluster/tlogs')
+
+cd('/Servers/MS3/DefaultFileStore/MS3')
+cmo.setDirectory(wlsDomain+'/SOACoreDomain/soa_cluster/tlogs')
+
+save()
+activate()
