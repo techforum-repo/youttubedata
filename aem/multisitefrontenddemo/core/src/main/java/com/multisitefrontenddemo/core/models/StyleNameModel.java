848d83051feb2f7ce096cafef460c24e22950ac9
techforum-repo <admin@albinsblog.com>
2021-02-14 00:08:29 -0600

    "Model change"

--- aem/multisitefrontenddemo/core/src/main/java/com/multisitefrontenddemo/core/models/StyleNameModel.java
+++ aem/multisitefrontenddemo/core/src/main/java/com/multisitefrontenddemo/core/models/StyleNameModel.java
@@ -1,33 +1,25 @@
 package com.multisitefrontenddemo.core.models;
 
-import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.models.annotations.Model;
 
-import com.day.cq.commons.inherit.InheritanceValueMap;
 
 @Model(adaptables = { SlingHttpServletRequest.class })
 public class StyleNameModel {
 
 	@Inject
-	private InheritanceValueMap pageProperties;
+	private String styleName;
 
-	Object styleName;
-
-	@PostConstruct
-	protected void init() {
-		styleName = pageProperties.get("styleName");
-	}
 
 	public String getStyleNameSite() {
 
-		return styleName != null ? styleName.toString().concat(".site") : "site1.site";
+		return styleName != null ? styleName.concat(".site") : "site1.site";
 	}
 
 	public String getStyleNameDependency() {
-		return styleName != null ? styleName.toString().concat(".dependencies") : "site1.dependencies";
+		return styleName != null ? styleName.concat(".dependencies") : "site1.dependencies";
 	}
 
 }
