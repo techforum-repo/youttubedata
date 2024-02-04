cdfbfacdd8e5e0d74cd262e345ebb2c54f39ab53
techforum-repo <admin@albinsblog.com>
2024-02-04 14:14:49 -0600

    Browser Communications

---
+++ browser/message-channel/app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const path = require('path')
+const https = require('https');
+const fs = require('fs');
+
+
+var app = express()
+
+
+app.get('/index.html', function (req, res) { 
+
+  res.sendFile('index.html', {
+        root: path.join(__dirname, '.')
+    })
+})
+
+app.get('/embed.html', function (req, res) { 
+
+  res.sendFile('embed.html', {
+        root: path.join(__dirname, '.')
+    })
+})
+
+
+
+
+https.createServer({
+  key: fs.readFileSync('ssl\\server.key'),
+  cert: fs.readFileSync('ssl\\server.crt')
+}, app)
+.listen(443, function () {
+  console.log('Example app listening on port 443! Go to https://localhost/')
+})
+
+
+
+
+
+
+
